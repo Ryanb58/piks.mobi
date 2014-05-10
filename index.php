65475af7387dfbd77edd9c022dd707b6b0db2d3d
Taylor Brazelton <ryanb58@live.com>
2014-05-10 02:30:21 -0400

    Added Filter to index.php

--- index.php
+++ index.php
@@ -2,22 +2,60 @@
 
 include('/template/header.php');
 
-$page = 0;
+?>
 
-if(isset($_GET['page']))
-{
-	$page = $_GET['page'];
-}
-else
-{
+
+
+<!-- Nav tabs -->
+<ul id="tabs" class="nav nav-tabs">
+	<li><a href="?sort=day">Day</a></li>
+	<li><a href="?sort=week">Week</a></li>
+<!--
+	<li><a href="#" data-toggle="tab">Month</a></li>
+	<li><a href="#" data-toggle="tab">Year</a></li>
+	<li><a href="#" data-toggle="tab">All</a></li>
+-->
+</ul>
+<?php
+
 	$page = 0;
-}
 
-$limit = 1;
-$start = $page * $limit;
+	if(isset($_GET['page']))
+	{
+		$page = $_GET['page'];
+	}
+	else
+	{
+		$page = 0;
+	}
 
-	$query = "SELECT * FROM pictures WHERE uploadedDate BETWEEN date_sub(now(),INTERVAL 1 WEEK) AND now() ORDER BY uploadedDate DESC LIMIT $start , $limit";
-	
+	$limit = 1;
+	$start = $page * $limit;
+
+	if(isset($_GET['sort']))
+	{
+		$policy = strtolower(mysql_real_escape_string($_GET['sort']));
+	}
+	else
+	{
+		$policy = 'week';
+	}
+
+	switch($policy)
+	{
+		case 'day':
+			$sortBy = 'DAY';
+			break;
+		case 'week':
+			$sortBy = 'WEEK';
+			break;
+		default:
+			$sortBy = 'WEEK';
+			break;
+	}
+
+	$query = "SELECT * FROM pictures WHERE uploadedDate BETWEEN date_sub(now(),INTERVAL 1 $sortBy) AND now() ORDER BY uploadedDate DESC LIMIT $start , $limit";
+
 	//execute query
 	try {
 	    $stmt   = $db->prepare($query);
@@ -29,18 +67,23 @@ $start = $page * $limit;
 	catch (PDOException $ex) {
 		echo 'ERROR: ' . $ex->getMessage();
 	}
-	
+
 	//Fetch each row invidually...
 	while($row = $stmt->fetch()) {
-        print_r($row);
+	    print_r($row);
+	    print_r($query);
 
-        ?>
-        	<section>
+	    ?>
+	    	<section>
 				<img src="uploads/<?php print $row['picName']; ?>" class="mainImg" />
 				<p> Upvotes: 1000 -- Downvotes: 20 </p>
 			</section>
-        <?php
-    }
+	    <?php
+	}
+
+?>	
+
+<?php
 
 include('/template/footer.php');
 ?>
--- template/header.php
+++ template/header.php
@@ -9,18 +9,22 @@ require_once("config.inc.php");
 <html>
 <head>
 	<title>TITLE</title>
-	
+
 	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen" />
 	<link href="css/bootstrap-theme.css" rel="stylesheet" />
 	
 	<link href="css/template.css" rel="stylesheet" />
+
+  <!-- Load all the javascript after... -->
+  <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+  <script type="test/javascript" src="js/bootstrap.min.js"></script>
+  
 	
 	<meta name="viewport" content="width=dvice-width, initial-scale=1.0" />
 	
 </head>
 <body>
 <div id="wrapper">
-
 	<!-- Static navbar -->
       <div class="navbar navbar-inverse" role="navigation">
         <div class="container-fluid">
@@ -43,6 +47,4 @@ require_once("config.inc.php");
           </div><!--/.nav-collapse -->
         </div><!--/.container-fluid -->
       </div>
-	
-
 	<div>
