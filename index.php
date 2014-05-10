2649a232a58d43c9671903e54330b024d625ebb6
Taylor Brazelton <ryanb58@live.com>
2014-05-10 01:13:01 -0400

    Updated index SELECT statement...
    date(weekly) and LIMITED... For pagination...

--- index.php
+++ index.php
@@ -2,11 +2,28 @@
 
 include('/template/header.php');
 
-	$query = "SELECT * FROM pictures";
+$page = 0;
+
+if(isset($_GET['page']))
+{
+	$page = $_GET['page'];
+}
+else
+{
+	$page = 0;
+}
+
+$limit = 1;
+$start = $page * $limit;
+
+	$query = "SELECT * FROM pictures WHERE uploadedDate BETWEEN date_sub(now(),INTERVAL 1 WEEK) AND now() ORDER BY uploadedDate DESC LIMIT $start , $limit";
 	
 	//execute query
 	try {
 	    $stmt   = $db->prepare($query);
+	    //$stmt->bindParam(':start', $start);
+	    //$stmt->bindParam(':lim', $limit);
+
 	    $result = $stmt->execute();
 	}
 	catch (PDOException $ex) {
