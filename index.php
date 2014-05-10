d7997538f5e57e4103f3a75ec6423df51e0c4334
Taylor Brazelton <ryanb58@live.com>
2014-05-09 21:52:24 -0400

    Started the project...
    added theme, bootstrap, etc...

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TITLE</title>
+	
+	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen" />
+	<link href="css/bootstrap-theme.css" rel="stylesheet" />
+	
+	<link href="css/template.css" rel="stylesheet" />
+	
+	<meta name="viewport" content="width=dvice-width, initial-scale=1.0" />
+	
+</head>
+<body>
+<div id="wrapper">
+
+	<!-- Static navbar -->
+      <div class="navbar navbar-inverse" role="navigation">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">piks.mobi</a>
+          </div>
+          <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li class="dropdown-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="./">Default</a></li>
+              <li><a href="../navbar-static-top/">Static top</a></li>
+              <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </div>
+	
+
+	<div>
+		<!-- Repeat for each picture.... -->
+			<section>
+				<img src="uploads/1.jpg" class="mainImg" />
+				<p> Upvotes: 1000 -- Downvotes: 20 </p>
+			</section>
+	</div>
+	
+</div>
+
+	<!-- Load all the javascript after... -->
+	<script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+	<script type="test/javascript" src="js/bootstrap.min.js"></script>
+	
+</body>
+</html>
